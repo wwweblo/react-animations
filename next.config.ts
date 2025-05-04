2aef2bd4216ea2b731a51eb435504a7a47d69870
Yarik <coffe1n21@gmail.com>
2025-05-04 17:30:35 +0700

    github actions fix 4

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     basePath: "/react-animations",
+    assetPrefix: '/react-animations',
     output: "export",  // <=== enables static exports
-    reactStrictMode: true
+    reactStrictMode: true,
+    // чтобы пути к страницам заканчивались слэшем
+    trailingSlash: true,
+    // отключаем оптимизацию изображений на стороне сервера
+    images: {
+        unoptimized: true,
+    },
 };
 
 export default nextConfig;

a2828a47d0d2914b92f5e10a60d7589c1f8a78bf
Yarik <coffe1n21@gmail.com>
2025-05-04 17:36:55 +0700

    github actions fix 5

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,23 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
+
+// В проде — true, в dev — false
+const isProd = process.env.NODE_ENV === 'production';
+// Имя вашего репозитория без слэша на конце
+const repo = '/react-animations';
 
 const nextConfig: NextConfig = {
-    basePath: "/react-animations",
-    assetPrefix: '/react-animations',
-    output: "export",  // <=== enables static exports
+    // Подкаталог только в production
+    basePath: isProd ? repo : '',
+    assetPrefix: isProd ? repo : '',
+
+    // Включаем статический экспорт
+    output: 'export',
     reactStrictMode: true,
-    // чтобы пути к страницам заканчивались слэшем
+
+    // Для SEO и GitHub Pages: все страницы со слэшем
     trailingSlash: true,
-    // отключаем оптимизацию изображений на стороне сервера
+
+    // Чтобы Next.js не пытался оптимизировать изображения на сервере
     images: {
         unoptimized: true,
     },

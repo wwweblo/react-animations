e399d2dc4f5cd5c14b1f49d58d74a45b3565833b
Yarik <coffe1n21@gmail.com>
2025-05-04 17:14:47 +0700

    github act ions

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    basePath: "/react-animations",
+    output: "export",  // <=== enables static exports
+    reactStrictMode: true
 };
 
 export default nextConfig;
